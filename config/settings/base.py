0d143fdb5485bdb6a3413d5b315b6463ce362adb
Kiragu-Maina <jumaliko711@gmail.com>
2024-03-08 20:16:04 +0300

    second commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from decouple import Csv, config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,15 +84,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USERNAME"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOSTNAME"),
-        "PORT": config("DB_PORT", cast=int),
+        "NAME": os.environ["PGNAME"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": int(os.environ["PGPORT"]),
     }
 }
 
